2763e37ece0bd58d3a167ed77a11808c557bd303
Nikhesh <nikhesh.reddy@outlook.com>
2023-03-15 15:20:30 +0530

    Nikki Added

---
+++ src/test/java/com/generalstore/android/test/TestClass.java
@@ -0,0 +1,26 @@
+package com.generalstore.android.test;
+
+import org.junit.Test;
+
+public class TestClass {
+
+	@Test
+	public void TestCalsses() {
+		System.out.println("1");
+	}
+	
+	@Test
+	public void TestCalsses1() {
+		System.out.println("2");
+	}
+	
+	@Test
+	public void TestCalsses2() {
+		System.out.println("3");
+	}
+	
+	@Test
+	public void TestCalsses3() {
+		System.out.println("4");
+	}
+}
